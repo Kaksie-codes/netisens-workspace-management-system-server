00d4a3b68375df28b0e9d602a31f21f07485f456
Nsikak Akpan <builtbykaksy@gmail.com>
2025-02-18 09:54:48 +0100

    Update generateTokenAndSetCookie.js

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -8,8 +8,7 @@ const generateTokenAndSetCookie = (res, userId) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         // secure: false,
-        sameSite: 'strict',
-        // sameSite: 'lax',
+        sameSite: 'strict',        
         maxAge: 3 * 24 * 60 * 60 * 1000 // 3 days in milliseconds
     })
 
